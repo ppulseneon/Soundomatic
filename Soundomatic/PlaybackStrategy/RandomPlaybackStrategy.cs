c31a8c2aec5dee029e74e07c162089c3538aa43c
lizwlw <homakelizaveta4@gmail.com>
2025-06-12 12:08:16 +0300

    Implementing pseudo-randomness for a strategy

--- Soundomatic/PlaybackStrategy/RandomPlaybackStrategy.cs
+++ Soundomatic/PlaybackStrategy/RandomPlaybackStrategy.cs
@@ -10,12 +10,12 @@ namespace Soundomatic.PlaybackStrategy;
 /// </summary>
 public class RandomPlaybackStrategy : IPlaybackStrategy
 {
-    private Random random = new Random();
+    private readonly Random random = new Random();
 
     /// <summary>
     /// Коллекция для хранения индексов последних воспроизводимых звуков по названиям наборов
     /// </summary>
-    private ConcurrentDictionary<string, Sound> indexLastPlayed = new ConcurrentDictionary<string, Sound>();
+    private readonly ConcurrentDictionary<string, Sound> _indexLastPlayed = new();
 
     /// <inheritdoc />
     public Sound SelectSound(string packName, IList<Sound> sounds)
@@ -27,21 +27,43 @@ public class RandomPlaybackStrategy : IPlaybackStrategy
 
         if (sounds.Count == 1)
         {
-            indexLastPlayed[packName] = sounds[0];
+            _indexLastPlayed[packName] = sounds[0];
             return sounds[0];
         }
 
-        indexLastPlayed.TryGetValue(packName, out Sound? lastPlayed);
-        Sound playSound;
+        _indexLastPlayed.TryGetValue(packName, out Sound? lastPlayed);
+        Sound? playSound = null;
 
-        do
+        if (lastPlayed is not null && random.Next(0, 2) == 0)
         {
-            int currentIndex = random.Next(sounds.Count);
-            playSound = sounds[currentIndex];
+            if (sounds.Contains(lastPlayed))
+            {
+                playSound = lastPlayed;
+                _indexLastPlayed[packName] = playSound;
+                return playSound;
+            }
+        }
 
-        } while (playSound == lastPlayed);
+        var maxAttempt = sounds.Count * 2;
 
-        indexLastPlayed[packName] = playSound;
+        for (int i = 0; i < maxAttempt; i++)
+        {
+            var currentIndexSound = random.Next(sounds.Count);
+            Sound currentSound = sounds[currentIndexSound];
+
+            if (currentSound != lastPlayed)
+            {
+                playSound = currentSound;
+                break;
+            }
+        }
+
+        if (playSound is null)
+        {
+            playSound = sounds[random.Next(sounds.Count)];
+        }
+
+        _indexLastPlayed[packName] = playSound;
         return playSound;
     }
 } 
