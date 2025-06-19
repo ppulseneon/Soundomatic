23327b7837c63ab758652bea0e6d3140da5f681b
lizwlw <homakelizaveta4@gmail.com>
2025-06-19 16:40:24 +0300

    Implementation collapsing the application from the tray

--- Soundomatic/Views/MainWindow.axaml.cs
+++ Soundomatic/Views/MainWindow.axaml.cs
@@ -7,5 +7,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Closing += MainWindowClosing;
+    }
+
+    /// <summary>
+    /// При закрытии окна скрывает его, но не снимает задачу с процесса
+    /// </summary>
+    private void MainWindowClosing(object? sender, WindowClosingEventArgs e)
+    {
+        e.Cancel = true;
+        Hide();
     }
 }
